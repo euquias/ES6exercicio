aeb48037618a0db7056d62baf8b47eac53fc5899
euquias <willmagalhaes25@gmail.com>
2022-08-18 11:45:22 -0300

    estudando params de functions

---
+++ function/function.js
@@ -0,0 +1,10 @@
+function /* nome propio */
+euquias() /* nome da function */
+{} /* pode receber parametros ou retorna valor */
+
+/* criar de forma leteral */
+function euquias(){}
+
+/* armazenda em uma variavel */
+const euquias2 = function(){}
+ 
