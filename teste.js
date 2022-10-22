c6746dd10e617777cd9e17e53aab3d034ce9378c
euquias <willmagalhaes25@gmail.com>
2022-10-22 13:13:11 -0300

    enviando pra branch teste

--- teste.js
+++ teste.js
@@ -1,12 +1,6 @@
-const geladeira = new Set()
 
-geladeira.add('carne')
-geladeira.add('frutas')
-geladeira.add('verdutas')
-geladeira.add('bebias')
 
-console.log(geladeira.delete('bebias'))
-console.log(geladeira)
-console.log(geladeira.add('bebidas'))
-console.log(geladeira.size)
-console.log(geladeira.has('bebidas'))
+git.add('alterando branche')
+
+console.log(git)
+
